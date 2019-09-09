3754100771e6c453d50660e829825c2cea47d975
Felix Tuffreaud <felix.tuffreaud@laposte.net>
2019-09-09 21:43:14 +0200

    add image directory, modify controller

--- src/Controller/CvController.php
+++ src/Controller/CvController.php
@@ -2,18 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\About;
+use App\Repository\AboutRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CvController extends AbstractController
 {
     /**
      * @Route("/", name="cv")
+     * @param AboutRepository $aboutRepository
+     * @return Response
      */
-    public function index()
+    public function index(AboutRepository $aboutRepository)
     {
+        $about = $aboutRepository->findOneBy(['id' => '1']);
+        /*$about = $this->getDoctrine()
+            ->getRepository(About::class)
+            ->findOneBy(['id' => '1']);*/
+
         return $this->render('cv/index.html.twig', [
-            'controller_name' => 'CvController',
+            'about' => $about,
         ]);
     }
 }
